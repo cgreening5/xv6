8d666302914c68e0ee89cd18e7b60d5150a05a83
root <root@localhost.localdomain>
2018-02-24 10:16:52 -0500

    Changed user test program.

--- testlazyalloc.c
+++ testlazyalloc.c
@@ -1,15 +1,62 @@
 #include "types.h"
 #include "user.h"
 #include "mmu.h"
-
 #define stdout 1
+
+#define PGSIZE 4096
+
+int canaccesspageboundaries(char * ptr)
+{
+	int testvalue = 0xDEADBEEF;
+	ptr -= (int) ptr % PGSIZE;
+	printf(stdout, "%x-", (int) ptr);
+	memmove(ptr, &testvalue, sizeof(int));
+	if (testvalue != *(int *)ptr)
+	{
+		return 0;
+	}
+	ptr += PGSIZE - 1;
+	printf(stdout, "%x\n", (int) ptr);
+	memmove(ptr, &testvalue, sizeof(int));
+	if (testvalue != *(int *)ptr)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+void test(int result)
+{
+	if (result)
+	{
+		printf(stdout, "Success.\n");
+	}
+	else
+	{
+		printf(stdout, "Failed.\n");
+	}
+}
 
 int main()
 {
-	for (int i = 0; i < 8; i++)
-	{
-		void * pointer = sbrk(PGSIZE + 1);
-		printf(stdout, "Memory allocated: %x\n", pointer);
-	}
+
+	char * ptr = sbrk(0);
+	printf(stdout, "Testing writing to page boundaries "
+		       	"at initial process size.\n");
+	test(canaccesspageboundaries(ptr));
+	
+	ptr = sbrk(PGSIZE + 1);
+
+	printf(stdout, "Growing process from %d to %d bytes.\n", ptr, ptr + PGSIZE + 1);
+
+	ptr += PGSIZE + 1; 
+	printf(stdout, "Testing writing to page boundaries "
+		       	"after growing process size.\n");
+	test(canaccesspageboundaries(ptr));
+	
+	ptr += PGSIZE;
+	printf(stdout, "Testing writing to page boundaries "
+		       "past process size.\n");
+	test(canaccesspageboundaries(ptr));
 	exit();
 }
