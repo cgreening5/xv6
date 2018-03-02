11ab557175e72a2729b3283414b4056dc6c4e3c7
Dave <umnewsod@myumanitoba.ca>
2018-03-01 19:55:56 -0600

    new stuff before merging chrisa3

--- proc.h
+++ proc.h
@@ -49,6 +49,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int lticks;  //ticks executed while on low priority
+  int hticks;  //ticks executed while on high priority
 };
 
 // Process memory is laid out contiguously, low addresses first:
--- ps.c
+++ ps.c
@@ -8,23 +8,23 @@
 
 int main()
 {
-	//int i = 0;
-	//pstat * ps = malloc(sizeof(pstat));
-	//int success = -1;
+	int i = 0;
+	pstat * ps = malloc(sizeof(pstat));
+	int success = -1;
 	
 
 	//printf(1,"%x\n",ps);
-	//success = getpinfo(ps);
-	//printf(1,"%x\n",ps);
+	success = getpinfo(ps);
+	printf(1,"%d\n",success);
 
 	
 	//success++;
-	/*
+
 	for(i = 0; i < NPROC; i++)
 	{
 		printf(1,"inuse: %d %d %d %d\n",ps->inuse[i],ps->pid[i],ps->hticks[i],ps->lticks[i]);		
 		
 	}
-	*/
+
 	exit();
 }
