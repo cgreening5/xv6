5410724be787b4f34b537bfe0bb006f3ac5ae794
Yahya Hamed <hamedm@myumanitoba.ca>
2018-03-17 21:26:57 -0500

    update filestat.c

--- filestat.c
+++ filestat.c
@@ -18,6 +18,6 @@ int main(int argc, char * argv[])
   fstat(fd, &st);
   printf(stdout, "filename=%s checksum: %d \n type: %d  \n size =%d \n device number =%d \n inode number =%d\n links to file =%d\n ",
          fileName,(int)st.checksum,(int)st.type,(int)st.size,st.dev,(int)st.ino,(int)st.nlink);
-  printf(stdout, "%s\n", filename);
+  //printf(stdout, "%s\n", filename);
   exit();
 }
