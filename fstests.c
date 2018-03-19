60937653c71d9e16aed6ee85ab0b48bf422ad038
root <root@localhost.localdomain>
2018-03-19 01:08:08 -0400

    Fixed test program.

--- fstests.c
+++ fstests.c
@@ -41,35 +41,42 @@ void testlargefile()
 {
   printf(stdout, "Creating new file.\n");
   int fd = open(FILENAME, O_CHECKED | O_WRONLY | O_CREATE);
+  char * teststring = "this is a test.";
+  int len = strlen(teststring);
   printfstat(fd);
 
   printf(stdout, "Writing to file.\n");
-  char * testmesg = "This is a test."; 
 
-  //fill buffer with whatever
+  //fill buffer with whatever. Too much repetition tends to cause
+  //0 checksums
   for (int i = 0; i < BSIZE; i++)
-    buffer[i] = 'A';
+    buffer[i] = teststring[i % len];
 
   for (int i = 0; i <= NDIRECT; i++)
   {
-    write(fd, testmesg, BSIZE);
-    printf(stdout, "%d blocks written of %d\n", i, NDIRECT);
+    write(fd, buffer, BSIZE);
   }
   printfstat(fd);
   
   printf(stdout, "Closing file.\n");
-  if (close(fd) < 0)
-    error("Unable to close file.");
 
   printf(stdout, "Opening file for reading\n.");
   fd = open(FILENAME, O_RDONLY);
   printfstat(fd);
 
-  printf(stdout, "Reading data from file\n");
-  read(fd, buffer, BSIZE);
+  printf(stdout, "Verifying data from file\n");
+  for (int i = 0; i <= NDIRECT; i++)
+  {
+    read(fd, buffer, BSIZE);
+    for (int i = 0; i < BSIZE; i++)
+    {
+      if (buffer[i] != teststring[i % len])
+      {
+        error("File corrupted.");
+      }
+    }
+  }
 
-  if (strcmp(buffer, testmesg) != 0)
-    error("Invalid memory.");
   printfstat(fd);
 
   close(fd);
@@ -80,12 +87,14 @@ void testsmallfile()
 {
   printf(stdout, "Testing writing to file.\n");
   char * teststring = "This is a test.";
-  char buffer[strlen(teststring + 1)];
-  int fd = open(FILENAME, O_WRONLY);
+  int len = strlen(teststring);
+  char buffer[len + 1];
+  int fd = open(FILENAME, O_WRONLY | O_CREATE | O_CHECKED);
   write(fd, teststring, strlen(teststring));
   printfstat(fd);
   close(fd);
-  fd = open(FILENAME, O_RDONLY);
+  
+  fd = open(FILENAME, O_RDONLY | O_CHECKED);
   read(fd, buffer, strlen(teststring));
   if (strcmp(teststring, buffer) == 0)
     printf(stdout, "Success\n.");
